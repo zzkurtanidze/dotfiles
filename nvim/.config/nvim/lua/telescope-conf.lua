7cea6a8469454334e96d81ed7d1320b89e8f0fc5
zura12337 <zura.kurta@gmail.com>
2022-02-11 13:00:04 +0400

    allow regex in telescope.

--- nvim/.config/nvim/lua/telescope-conf.lua
+++ nvim/.config/nvim/lua/telescope-conf.lua
@@ -7,5 +7,20 @@ require('telescope').setup({
     sorting_strategy = "ascending",
     prompt_prefix = '   ',
     path_display = {"smart"},
+    dynamic_preview_title = true,
+    history = {
+      mappings = {
+        i = {
+          ["<C-Down>"] = require('telescope.actions').cycle_history_next,
+          ["<C-Up>"] = require('telescope.actions').cycle_history_prev,
+        },
+      }
+    }
   },
+  grep_string = {
+    use_regex = true
+  },
+  live_grep = {
+    use_regex = true
+  }
 })
