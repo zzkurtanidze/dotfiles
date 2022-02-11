7584fed484bca0d2c10c09b1ce5878331ed95061
zura12337 <zura.kurta@gmail.com>
2022-02-11 12:59:00 +0400

    refactor lsp config.

--- nvim/.config/nvim/lua/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp-config.lua
@@ -6,118 +6,33 @@ local on_attach = function(client, bufnr)
 
   local opts = { noremap = true, silent = true }
 
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<leader>df', '<cmd>lua vim.diagnostic.goto_next({float = {border = "rounded"}})<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 
-  if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
-
-  local ts_utils = require("nvim-lsp-ts-utils")
-  ts_utils.setup({
-    eslint_bin = "eslint_d",
-    eslint_enable_diagnostics = true,
-    eslint_enable_code_actions = true,
-    enable_formatting = true,
-    formatter = "prettier",
-  })
-
-  ts_utils.setup_client(client)
 end
 
-nvim_lsp.intelephense.setup({
-    settings = {
-        intelephense = {
-            stubs = { 
-                "bcmath",
-                "bz2",
-                "calendar",
-                "Core",
-                "curl",
-                "zip",
-                "zlib",
-                "wordpress",
-                "woocommerce",
-                "acf-pro",
-                "wordpress-globals",
-                "wp-cli",
-                "genesis",
-                "polylang"
-            },
-            files = {
-                maxSize = 5000000;
-            };
-        };
-    }
-});
+local function clip_text(diagnostic)
+  -- whatever truncation logic you want
+  if #diagnostic.message < 70 then
+    return diagnostic.message
+  else
+    return string.sub(diagnostic.message, 1, 70) .. "... <leader>d for more info."
+  end
+end
 
-nvim_lsp.diagnosticls.setup {
-  on_attach = on_attach,
-  filetypes = { 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown' },
-  init_options = {
-    linters = {
-      eslint = {
-        command = 'eslint_d',
-        rootPatterns = { '.git' },
-        debounce = 100,
-        args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
-        sourceName = 'eslint_d',
-        parseJson = {
-          errorsRoot = '[0].messages',
-          line = 'line',
-          column = 'column',
-          endLine = 'endLine',
-          endColumn = 'endColumn',
-          message = '[eslint] ${message} [${ruleId}]',
-          security = 'severity'
-        },
-        securities = {
-          [2] = 'error'
-        }
-      },
-    },
-    filetypes = {
-      javascript = 'eslint',
-      javascriptreact = 'eslint',
-      typescript = 'eslint',
-      typescriptreact = 'eslint',
-    },
-    formatters = {
-      eslint_d = {
-        command = 'eslint_d',
-        args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
-        rootPatterns = { '.git' },
-      },
-      prettier = {
-        command = 'prettier',
-        args = { '--stdin-filepath', '%filename' }
-      }
-    },
-    formatFiletypes = {
-      css = 'prettier',
-      javascript = 'eslint_d',
-      javascriptreact = 'eslint_d',
-      json = 'prettier',
-      scss = 'prettier',
-      less = 'prettier',
-      typescript = 'eslint_d',
-      typescriptreact = 'eslint_d',
-      json = 'prettier',
-      markdown = 'prettier',
-    }
-  }
-}
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  { border = "rounded" }
+)
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
     virtual_text = {
       spacing = 5,
-      prefix = ''
+      prefix = '',
+      format = clip_text
     },
     update_in_insert = true,
   }
@@ -126,26 +41,174 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
-    disable = {},
   },
   indent = {
-    enable = false,
-    disable = {},
+    enable = true,
   },
   ensure_installed = {
+    "javascript",
     "tsx",
-    "fish",
     "php",
     "json",
     "yaml",
     "html",
-    "scss"
+    "css"
   },
 }
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.used_by = { "javascript", "typescript.tsx" }
 
-nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
-} 
+local servers = {
+  cssls = {},
+  tsserver = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  },
+  jsonls = {
+    filetypes = {"json", "jsonc"},
+    settings = {
+      json = {
+        -- Schemas https://www.schemastore.org
+        schemas = {
+          {
+            fileMatch = {"package.json"},
+            url = "https://json.schemastore.org/package.json"
+          },
+          {
+            fileMatch = {"tsconfig*.json"},
+            url = "https://json.schemastore.org/tsconfig.json"
+          },
+          {
+            fileMatch = {
+              ".prettierrc",
+              ".prettierrc.json",
+              "prettier.config.json"
+            },
+            url = "https://json.schemastore.org/prettierrc.json"
+          },
+          {
+            fileMatch = {".eslintrc", ".eslintrc.json"},
+            url = "https://json.schemastore.org/eslintrc.json"
+          },
+          {
+            fileMatch = {".babelrc", ".babelrc.json", "babel.config.json"},
+            url = "https://json.schemastore.org/babelrc.json"
+          },
+          {
+            fileMatch = {"lerna.json"},
+            url = "https://json.schemastore.org/lerna.json"
+          },
+          {
+            fileMatch = {"now.json", "vercel.json"},
+            url = "https://json.schemastore.org/now.json"
+          },
+          {
+            fileMatch = {
+              ".stylelintrc",
+              ".stylelintrc.json",
+              "stylelint.config.json"
+            },
+            url = "http://json.schemastore.org/stylelintrc.json"
+          }
+        }
+      }
+    }
+  },
+  yamlls = {
+    on_attach = on_attach,
+    capabilities = capabilities
+  },
+  diagnosticls = {
+    filetypes = { 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'markdown' },
+    init_options = {
+      -- linters = {
+      --   eslint = {
+      --     command = 'eslint_d',
+      --     rootPatterns = { '.git' },
+      --     debounce = 100,
+      --     args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
+      --     sourceName = 'eslint_d',
+      --     parseJson = {
+      --       errorsRoot = '[0].messages',
+      --       line = 'line',
+      --       column = 'column',
+      --       endLine = 'endLine',
+      --       endColumn = 'endColumn',
+      --       message = '[eslint] ${message} [${ruleId}]',
+      --       security = 'severity'
+      --     },
+      --     securities = {
+      --       [2] = 'error'
+      --     }
+      --   },
+      -- },
+      filetypes = {
+        javascript = 'eslint',
+        javascriptreact = 'eslint',
+        typescript = 'eslint',
+        typescriptreact = 'eslint',
+      },
+    }
+  }
+}
+
+for server, config in pairs(servers) do
+  local server_disabled = (config.disabled ~= nil and config.disabled) or false
+
+  if not server_disabled then
+    nvim_lsp[server].setup(
+      vim.tbl_deep_extend("force", {on_attach = on_attach, capabilities = capabilities}, config)
+    )
+  end
+end
+
+local null_ls = require("null-ls")
+local prettier = require("prettier")
+
+null_ls.setup({
+  on_attach = function(client, bufnr)
+    if client.resolved_capabilities.document_formatting then
+      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+      -- format on save
+      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
+    end
+
+    if client.resolved_capabilities.document_range_formatting then
+      vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+      vim.cmd("")
+    end
+  end,
+})
+
+prettier.setup({
+  bin = 'prettier',
+  filetypes = {
+    "css",
+    "graphql",
+    "html",
+    "javascript",
+    "javascriptreact",
+    "json",
+    "less",
+    "markdown",
+    "scss",
+    "typescript",
+    "typescriptreact",
+    "yaml",
+  },
+
+  arrow_parens = "always",
+  bracket_spacing = true,
+  embedded_language_formatting = "auto",
+  end_of_line = "lf",
+  html_whitespace_sensitivity = "css",
+  jsx_bracket_same_line = false,
+  jsx_single_quote = false,
+  print_width = 80,
+  prose_wrap = "preserve",
+  quote_props = "as-needed",
+  semi = true,
+  single_quote = false,
+  tab_width = 2,
+  trailing_comma = "es5",
+  use_tabs = false,
+  vue_indent_script_and_style = false,
+})
